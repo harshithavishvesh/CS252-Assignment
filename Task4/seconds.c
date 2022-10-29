6ba0ca24eb6c204c9bf7e16c711789dbc9683436
harshithavishvesh <100205213+harshithavishvesh@users.noreply.github.com>
2022-10-29 22:16:04 +0530

    Update seconds.c

--- Task4/seconds.c
+++ Task4/seconds.c
@@ -11,14 +11,14 @@
 
 unsigned long start_jiffies = 0;
 
-static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
 
 static struct proc_ops my_procop= {
     .proc_read = proc_read,
 };
 
 /* This function is called when the module is loaded. */
-static int proc_init(void)
+int proc_init(void)
 {
     // creates the /proc/seconds entry
     proc_create(PROC_NAME, 0666, NULL, &my_procop);
@@ -27,14 +27,14 @@ static int proc_init(void)
 }
 
 /* This function is called when the module is removed. */
-static void proc_exit(void)
+void proc_exit(void)
 {
     // removes the /proc/seconds entry
     remove_proc_entry(PROC_NAME, NULL);
 }
 
 /*This function is called each time the /proc/seconds is read. */
-static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
     char buffer[BUFFER_SIZE];
     int rv = 0;
